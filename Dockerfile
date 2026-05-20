e7dd1699dd6027497fc775aa18968c342f6c82b9
venkats56 <venkatvs2468@gmail.com>
2026-05-20 22:49:37 +0530

    added the changes

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM python:3.12-alpine
+COPY requirements.txt /tmp
+RUN pip install -r /tmp/requirements.txt
+COPY ./src /src
+CMD  python /src/app.py
---
+++ src/app.py
@@ -0,0 +1,20 @@
+# '/api/v1/details'
+# '/api/v1/healthz'
+from flask import Flask,jsonify
+import datetime
+import socket
+app = Flask(__name__)
+
+@app.route('/api/v1/details')
+
+def details():
+    return jsonify({'host': socket.gethostname(),
+                    'timestamp': datetime.datetime.now().isoformat()
+    })
+
+@app.route('/api/v1/healthz')
+def healthz():
+    return jsonify({'status': 'healthy'}),200
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
